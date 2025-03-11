21a6107ffbd5c05d0758603a499e347ab67313ee
mcconvillejoannie639 <mcconvillejoannie639@gmail.com>
2025-03-11 06:19:38 +0000

    Add 6.java

---
+++ src/6.java
@@ -0,0 +1,11 @@
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        System.out.println("This is a random Java program.");
+        int x = 5;
+        if (x > 3) {
+            System.out.println("The value of x is greater than 3.");
+        } else {
+            System.out.println("The value of x is less than or equal to 3.");
+        }
+    }
+}
